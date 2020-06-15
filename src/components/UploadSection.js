33c79bebc56efb439667d1d8f725e8bb0049ee06
parth-v <parthvashisht20@gmail.com>
2020-06-16 00:43:41 +0530

    added uploading func

--- src/components/UploadSection.js
+++ src/components/UploadSection.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import './UploadStyle.css';
 
 const UploadSection = () => {
 	return (
-		<h2>Upload a new File</h2>
+		<div>
+			<h2>Upload a new File</h2>
+			<div class="container">
+				<div>
+		      <form method="post" action="#" id="#">
+            <div class="form-group files">
+              <label>Upload Your File </label>
+              <input type="file" class="form-control" multiple="" />
+            </div>
+          </form>
+				</div>
+			</div>
+		</div>
 	);
 };
 
