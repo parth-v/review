a1cb53d929bb258703e1adddf0a3e054410d9ac5
parth-v <parthvashisht20@gmail.com>
2020-06-19 00:47:34 +0530

    added input validations and missing files

--- src/components/UploadSection.js
+++ src/components/UploadSection.js
@@ -4,9 +4,13 @@ import './UploadStyle.css';
 
 const UploadSection = () => {
 	const [ file,setFile ] = useState(null);
-	let onChangeHandler = (event) => {
-		setFile(event.target.files);
+	
+	let onChangeHandler = event => {
+		if(maxSelectFile(event) && checkMimeType(event)) {
+			setFile(event.target.files);
+		}
 	}
+
 	let onClickHandler = () => {
     const data = new FormData();
     for(let i = 0; i < file.length; i++) {
@@ -19,6 +23,34 @@ const UploadSection = () => {
 		});
 	}	
 
+	let maxSelectFile = event => {
+    let files = event.target.files;
+	  if (files.length > 3) { 
+	    const msg = 'Only 3 files can be uploaded at a time!';
+	    event.target.value = null;
+	    console.log(msg);
+	    return false;
+    }
+    return true;
+	}
+
+	let checkMimeType = event => {
+  	let files = event.target.files; 
+ 	 	let err = '';
+	  const types = ['application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'];
+	  for(let i = 0; i < files.length; i++) {
+	    if (types.every(type => files[i].type !== type)) {
+	      err = 'File format not supported!\n';
+	    }
+	  };
+	  if (err !== '') { 
+	    event.target.value = null;
+	    console.log(err);
+	    return false; 
+	  }
+	  return true;
+	}
+
 	return (
 		<div>
 			<h2>Upload a new File</h2>
@@ -27,10 +59,10 @@ const UploadSection = () => {
 		      <form method="post" action="#" id="#">
             <div className="form-group files">
               <label>Upload Your File </label>
-              <input type="file" style={{cursor: 'pointer'}} name="file" multiple onChange={onChangeHandler}/>
+              <input type="file" style={{cursor: 'pointer'}} accept=".pdf, .docx, .doc" name="file" multiple onChange={onChangeHandler}/>
             </div>
           </form>
-					<button type="button" class="btn btn-success btn-block" onClick={onClickHandler}>Upload</button>
+					<button type="button" className="btn btn-success btn-block" onClick={onClickHandler}>Upload</button>
 				</div>
 			</div>
 		</div>
