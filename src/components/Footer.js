1615686fb9385a6af69f9d21b07ce63edb20dd69
Parth_V <parthvashisht20@gmail.com>
2020-08-05 01:38:48 +0530

    improved footer and comment section UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer>
-      <div className="text-center bg-secondary" style={{paddingTop: '10px',height: '75px'}}>
+      <div className="text-center text-white bg-dark" style={{paddingTop: '10px',height: '75px'}}>
         Terms & Conditions
         © E-Fermat 2020
       </div>
--- src/components/comments/Comment.js
+++ src/components/comments/Comment.js
@@ -6,17 +6,18 @@ export default function Comment(props) {
   return (
     <div className="media mb-3">
       <img
-        className="mr-3 bg-light rounded"
+        className="mr-3 bg-dark border border-primary rounded"
         width="48"
         height="48"
         src={avatar}
         alt={name}
       />
 
-      <div className="media-body p-2 shadow-sm rounded bg-light border">
+      <div className="media-body p-2 shadow-sm rounded bg-light border border-secondary">
         <small className="float-right text-muted">{time}</small>
-        <h6 className="mt-0 mb-1 text-muted">{name}</h6>
-        {message}
+        <h6 className="float-left mt-0 mb-1 text-muted">{name}</h6>
+        <br/>
+        <p className="float-left">{message}</p>
       </div>
     </div>
   );
