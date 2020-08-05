6fa8f5783a09119a2e118f5df0ddfd24a780c862
Parth_V <parthvashisht20@gmail.com>
2020-08-05 13:19:05 +0530

    added app authentication

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
 
-const PrivateRoute = (props) => {
-	const isAuthenticated = true;
+const PrivateRoute = ({component: Component, ...rest}) => {
+	const isAuthenticated = false;
 	return (
-		isAuthenticated ? (
-			<div>Allowed</div>
-		) : (
-			<div>Not allowed</div>
-		)
+		<Route 
+			{...rest}
+			render={ props => 
+				isAuthenticated ? (
+					<Component {...props} />
+				) : (
+					<Redirect to={{ pathname: "/signin", state: { from: props.location} }} />
+				)
+			}
+		/>
 	);
 };
 
