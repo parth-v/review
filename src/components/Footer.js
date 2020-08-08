de001cbf7236a3d78178754dee78ee3fa5b14a0d
Parth_V <parthvashisht20@gmail.com>
2020-08-08 23:58:51 +0530

    modifying footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<footer>
-      <div className="text-center text-white bg-dark" style={{paddingTop: '10px',height: '75px'}}>
-        Terms & Conditions
-        © E-Fermat 2020
+      <div className="text-center text-white bg-dark p-2">
+        <p>© Copyright 2020 FERMAT | All Rights Reserved</p>
+        <p>"FERMAT is published under the auspices of the University of Central Florida"</p>
+				<p>ISSN 2470-4202</p>
       </div>
     </footer>
 	);
